fa2da9de4a38bf794ade97c1c0663fb77fa87e3f
KSevery <konstantinsevery@gmail.com>
2016-08-25 11:19:27 +0300

    New project for showing conversion between types

---
+++ Examples.Converting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples.Converting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+
+            float floatNum = num;
+
+            floatNum += 0.15f;
+
+            Console.WriteLine(floatNum);
+
+            string lucho = "15";
+
+            int luchoNum;
+            
+            if (int.TryParse(lucho, out luchoNum))
+            {
+                Console.WriteLine("lucho " + luchoNum);
+            }
+            else
+            {
+                Console.WriteLine("Not a number!");
+            }
+        }
+    }
+}
